d59f0f2f030d0a85ea4aa9d6632130bff627bf1e
WildSteph <Stephaine.Pantiez@gmail.com>
2020-04-29 16:20:11 +0200

    refactor(app.jsx): add axios for objectID in app.Jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
-import NavBar from './components/NavBar';
 import Axios from 'axios';
 
 function App() {
   const [departments, setDepartments] = useState([]);
+  const [objectIDs, setObjectIDs] = useState([]);
+  const [wantedDepartment, setWantedDepartment] = useState(null);
 
   useEffect(() => {
     Axios.get('https://collectionapi.metmuseum.org/public/collection/v1/departments')
@@ -15,11 +16,22 @@ function App() {
       });
   }, []);
 
+  useEffect(() => {
+    if (wantedDepartment != null) {
+      Axios.get(`https://collectionapi.metmuseum.org/public/collection/v1/search?departmentId=${wantedDepartment}&q=cat`)
+        .then((response) => response.data)
+        .then((data) => {
+          //setObjectIDs(data.objectIDs ?? []);
+          setObjectIDs(data.objectIDs != null ? data.objectIDs : []);
+        });
+    }
+  }, [wantedDepartment]);
+
   return (
     <div className="App">
-      {departments.map((department) => (<p>{ department.displayName }</p>))}
+      {departments.map((department) => (<button type="button" onClick={() => setWantedDepartment(department.departmentId)}>{department.displayName}</button>))}
+      {objectIDs.map((objectID) => (<p>{objectID}</p>))}
     </div>
   );
 }
-
 export default App;
